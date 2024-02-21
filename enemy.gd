3a43bbd5cce17cdc90e67f1bca136396587f0b1b
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-02-21 19:11:32 +0800

    finally fixed animation toggle(?) and enemy-player collision

--- enemy.gd
+++ enemy.gd
@@ -8,10 +8,11 @@ var facing =  1
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
-	velocity.x = facing * speed
-	$Sprite2D.flip_h = velocity.x > 0
+	# velocity.x = facing * speed
+	# $Sprite2D.flip_h = velocity.x > 0
 	
 	move_and_slide()
+	
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		if collision.get_collider().name == "Player":
@@ -31,5 +32,5 @@ func take_damage():
 
 
 func _on_animation_player_animation_finished(anim_name):
-	if anime_name == "enemy_death":
+	if anim_name == "enemy_death":
 		queue_free()
